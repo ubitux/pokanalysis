51a1d4af8eeb691d9020316db32d285ae73fcdaf
Clément Bœsch <ubitux@gmail.com>
2010-09-03 20:14:25 +0200

    Drop useless addr methods

--- libpokerom/pokerom.c
+++ libpokerom/pokerom.c
@@ -23,36 +23,6 @@
 u8 *gl_stream;
 struct stat gl_rom_stat;
 
-PyObject *read_addr(PyObject *self, PyObject *args)
-{
-	int offset = 0;
-	int addr, rom_addr;
-
-	PyArg_ParseTuple(args, "i", &offset);
-	addr = GET_ADDR(offset);
-	rom_addr = ROM_ADDR(offset / 0x4000, addr);
-	return Py_BuildValue("ii", addr, rom_addr);
-}
-
-static PyObject *read_data(PyObject *self, PyObject *args)
-{
-	u8 *s;
-	int offset;
-	PyObject *list = PyList_New(0);
-
-	PyArg_ParseTuple(args, "is", &offset, &s);
-	for (; *s; s++) {
-		if (*s == 'B') { /* 8-bit */
-			PyList_Append(list, Py_BuildValue("i", gl_stream[offset]));
-			offset++;
-		} else if (*s == 'W') { /* 16-bit */
-			PyList_Append(list, read_addr(NULL, Py_BuildValue("(i)", offset, NULL)));
-			offset += 2;
-		}
-	}
-	return list;
-}
-
 static PyObject *load_rom(PyObject *self, PyObject *args)
 {
 	char *fname;
@@ -79,8 +49,6 @@ PyMODINIT_FUNC initpokerom(void)
 		/* Utils */
 		{"str_getbin", str_getbin, METH_VARARGS, "Convert binary text to ascii"},
 		{"str_getascii", str_getascii, METH_VARARGS, "Convert ascii text to binary"},
-		{"read_addr", read_addr, METH_VARARGS, "Get 24-bits ROM address from 16-bit address read at the given offset"},
-		{"read_data", read_data, METH_VARARGS, "Return list of 8-bit char and 16-bit address (same return as read_addr)"},
 		{NULL, NULL, 0, NULL}
 	};
 	Py_InitModule("pokerom", m);
