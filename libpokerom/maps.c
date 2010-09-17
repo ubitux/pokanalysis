acedd20cdb93c4631415347b51ec68f43b9ece1c
Clément Bœsch <ubitux@gmail.com>
2010-09-17 15:03:26 +0200

    Fix item id misinterpreted for trainer pkmn set

--- libpokerom/maps.c
+++ libpokerom/maps.c
@@ -478,7 +478,7 @@ static void set_map_things_in_python_dict(PyObject *dict, struct map_things *thi
 			PyDict_SetItemString(py_entity, "trainer_type", Py_BuildValue("i", data->extra_1));
 			PyDict_SetItemString(py_entity, "pkmn_set", Py_BuildValue("i", data->extra_2));
 		} else if (strcmp(entity->type, "item") == 0) {
-			PyDict_SetItemString(py_entity, "pkmn_set", Py_BuildValue("i", data->extra_1));
+			PyDict_SetItemString(py_entity, "item_id", Py_BuildValue("i", data->extra_1));
 		}
 		PyList_Append(list, py_entity);
 		entity = entity->next;
