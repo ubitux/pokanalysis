08b587968d51d73e3d3f2b04cd318b1252b14fb5
Clément Bœsch <ubitux@gmail.com>
2011-03-23 19:50:31 +0100

    Remove CMake dependency and use Python distutils instead

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import glob
+from distutils.core import setup, Extension
+
+setup(
+    name='pokerom',
+    version='0.6~dev',
+    description='Pokémon ROM Red & Blue (US) analysis module',
+    author='Clément Bœsch',
+    author_email='ubitux@gmail.com',
+    url='http://pokanalysis.ubitux.fr',
+    license='GPLv3',
+    ext_modules=[Extension(
+        'pokerom',
+        sources=glob.glob('libpokerom/*.c'),
+        extra_compile_args=['-Wall', '-Wextra', '-O3', '-std=c99'],
+    )],
+)
