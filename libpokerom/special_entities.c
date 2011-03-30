ea9b1a3620969f1ebfcc278ff77f1c45e0260f47
Clément Bœsch <ubitux@gmail.com>
2011-03-31 01:27:03 +0200

    Differenciate hidden items special entities from the others

--- libpokerom/special_entities.c
+++ libpokerom/special_entities.c
@@ -61,22 +61,23 @@ void apply_filter(u8 *stream, u8 *pixbuf, int map_id, int w)
 		int y = stream[item_addr];
 		int x = stream[item_addr + 1];
 		int offset = (y * 16 * (w * 16) + x * 16) * 3;
+		char *c = stream[item_addr + 3] == 0x1d ? "\xff\x00\x00" : "\x00\xff\x00";
 		int i;
 
 		if (x == 0xff || y == 0xff)
 			return;
 
 		for (i = 0; i < 16; i++)
-			memcpy(&pixbuf[offset + i * 3], "\xFF\x00\x00", 3);
+			memcpy(&pixbuf[offset + i * 3], c, 3);
 
 		for (i = 0; i < 16; i++)
-			memcpy(&pixbuf[offset + (w * 16 * 3) * i], "\xFF\x00\x00", 3);
+			memcpy(&pixbuf[offset + (w * 16 * 3) * i], c, 3);
 
 		for (i = 0; i < 16; i++)
-			memcpy(&pixbuf[offset + (w * 16 * 3) * i + 15 * 3], "\xFF\x00\x00", 3);
+			memcpy(&pixbuf[offset + (w * 16 * 3) * i + 15 * 3], c, 3);
 
 		offset += (w * 16 * 3) * 15;
 		for (i = 0; i < 16; i++)
-			memcpy(&pixbuf[offset + i * 3], "\xFF\x00\x00", 3);
+			memcpy(&pixbuf[offset + i * 3], c, 3);
 	}
 }
