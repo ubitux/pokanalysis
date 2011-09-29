5085188fac2d90cee09473d3f5a9d3a2229ec1d9
Clément Bœsch <ubitux@gmail.com>
2011-09-29 22:39:55 +0200

    tests: prefix pkmn and trainers tests.

--- tests/tests.py
+++ tests/tests.py
@@ -9,12 +9,12 @@ crc32 = lambda x: binascii.crc32(x) & 0xffffffff
 rom = pokerom.ROM(sys.argv[1])
 dex = rom.get_pokedex()
 for pkmn in dex:
-    print '%-12s %08X %08X' % (pkmn['name'], crc32(pkmn['pic'][0]), crc32(pkmn['pic'][1]))
+    print 'PKMN %-12s %08X %08X' % (pkmn['name'], crc32(pkmn['pic'][0]), crc32(pkmn['pic'][1]))
 
 trainers = rom.get_trainers()
 for trainer in trainers:
     if 'name' in trainer:
-        print '%-12s %08X' % (trainer['name'], crc32(trainer['pic']))
+        print 'TRAINER %-12s %08X' % (trainer['name'], crc32(trainer['pic']))
 
 maps = rom.get_maps()
 for m in maps:
