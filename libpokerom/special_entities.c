342c511f994cbf8225a0d20b0145bd57dde6f254
Clément Bœsch <ubitux@gmail.com>
2012-02-02 06:08:00 +0100

    special_entities: add a special case for casino coins.

--- libpokerom/special_entities.c
+++ libpokerom/special_entities.c
@@ -36,10 +36,12 @@ PyObject *get_special_items(u8 *stream, int map_id)
         u8 *item_data = &stream[ROM_ADDR(0x11, GET_ADDR(ROM_ADDR(0x11, 0x6a96 + idx)))];
         while (item_data[0] != 0xff) {
             u16 addr = *(u16*)&item_data[4];
-            if (item_data[3] == 0x1d && (addr == 0x6688 || addr == 0x6689)) {
+            if (item_data[3] == 0x1d) {
                 char iname[30];
-
-                get_item_name(stream, iname, item_data[2], sizeof(iname));
+                if (addr == 0x6688 || addr == 0x6689)
+                    get_item_name(stream, iname, item_data[2], sizeof(iname));
+                else
+                    snprintf(iname, sizeof(iname), "%d coins", item_data[0]);
                 PyList_Append(list, Py_BuildValue("iisii", item_data[0], item_data[1], iname, item_data[3], addr));
             } else {
                 PyList_Append(list, Py_BuildValue("iiiii", item_data[0], item_data[1], item_data[2], item_data[3], addr));
